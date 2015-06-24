4196fef262c4e89bc6c9a6274153f2a77138853f
Michael Murphy <murphym18@gmail.com>
2015-06-23 21:39:23 -0700

    made work with a list of cats

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
 var App = new Mn.Application();
-
+App.update
 
 App.on('start', function() {
-  var cats = new CatCollection([{name: "Fuzzy"}, {name: "Ash"}]);
+  var cats = new CatCollection([
+    {name: "Fuzzy", image: 'images/cat1.jpg'},
+    {name: "Ash", image: 'images/cat2.jpg'},
+    {name: "Kitty", image: 'images/cat3.jpg'},
+    {name: "Chi Chi", image: 'images/cat4.jpg'},
+    {name: "Whiskers", image: 'images/cat5.jpg'},
+  ]);
+  var firstCat = cats.at(0);
+  cats.on('select', function(cat) {
+    App.getRegion('main').show(new CatView({model: cat}));
+  })
   var catsView = new CatCollectionView({collection: cats});
-  App.getRegion('main').show(catsView);
+  App.getRegion('list').show(catsView);
+  cats.trigger('select', firstCat);
 });
 
 
@@ -12,7 +23,8 @@ App.on('start', function() {
 
   $(function(){
     App.addRegions({
-      main: "#cats",
+      list: "#list",
+      main: "#main"
     });
     App.start();
   });
--- js/cat-view.js
+++ js/cat-view.js
@@ -17,7 +17,44 @@
 
   });
 
+  app.CatListItemView = Marionette.ItemView.extend({
+    tagName: "button",
+    className:"list-group-item ",
+    template: false,
+    events: {
+      "click": "updateActive"
+    },
+    modelEvents: {
+      "change": "onShow"
+    },
+    collectionEvents: {
+      "pick:cat": "updateActive" // equivalent to view.listenTo(view.collection, "add", view.itemAdded, view)
+    },
+    initialize: function() {
+      this.onIncrement = this.model.increment.bind(this.model);
+      this.listenTo(this.model.collection, 'select', this.updateSelected, this);
+    },
+    onShow: function() {
+      this.$el.attr('type', 'button');
+      this.$el.text(this.model.get('name'));
+
+    },
+    updateSelected: function(cat) {
+      if (this.model === cat) {
+        this.$el.addClass('active');
+      }
+      else {
+        this.$el.removeClass('active');
+      }
+    },
+    updateActive: function(item) {
+      this.model.collection.trigger("select", this.model);
+    }
+  });
+
   app.CatCollectionView = Marionette.CollectionView.extend({
-    childView: CatView
+    className: "list-group",
+    tagName: "div",
+    childView: app.CatListItemView
   });
 })(window);
